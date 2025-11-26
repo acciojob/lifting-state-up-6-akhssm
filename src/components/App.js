3f18563a8dcd21d87481795708a654095bdb3b7b
Akshay <mandaakshay20@gmail.com>
2025-11-26 15:09:40 +0530

    Update.

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,12 @@ import "./../styles/App.css";
 import TodoList from "./TodoList";
 
 const App = () => {
-  // Parent State
   const [todos, setTodos] = useState([
     { id: 1, text: "Learn React", completed: false },
-    { id: 2, text: "Practice Lifting State Up", completed: false },
-    { id: 3, text: "Build Projects", completed: false }
+    { id: 2, text: "Build a React app", completed: false },
+    { id: 3, text: "Deploy the React app", completed: false }
   ]);
 
-  // Function to mark todo as completed
   const handleComplete = (id) => {
     const updated = todos.map((item) =>
       item.id === id ? { ...item, completed: true } : item
