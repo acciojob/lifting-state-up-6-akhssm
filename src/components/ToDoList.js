4fb67d4420e03c85b71d92d443ec697bd4dfd551
Akshay <mandaakshay20@gmail.com>
2025-11-26 15:50:55 +0530

    Corrected.

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -4,7 +4,7 @@ const ToDoList = ({ todos, handleComplete }) => {
   return (
     <ul>
       {todos.map((todo) => (
-        <li key={todo.id}>
+        <li key={todo.id} style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
           {todo.text}
           {!todo.completed && (
             <button onClick={() => handleComplete(todo.id)}>Complete</button>
